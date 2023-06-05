848001037faded0a3dcb6e83ba911a72fabcbe8e
Giacomo Moggioli <112566383+Moggiacomo@users.noreply.github.com>
2023-06-05 15:38:40 +0200

    Create 1.1_BAM2fastx.sh

---
+++ 00_Assembly_Annotation/1.1_BAM2fastx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -pe smp 4
+#$ -l h_vmem=1G
+#$ -l h_rt=72:0:0
+
+#load the environment
+module load anaconda2
+source activate BAM2fastx_env
+bam2fastq \
+#output_name
+-o Oasisia_pb_raw \
+#path to the subreads.bam file received from PacBio
+pb/r64044_20190812_215729/1_A01/m64044_190812_220643.subreads.bam
