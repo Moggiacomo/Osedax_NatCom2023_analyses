91ae5ab0570ba6e3d9bd0db734fb402f4d8782f6
Giacomo Moggioli <112566383+Moggiacomo@users.noreply.github.com>
2023-06-05 15:52:36 +0200

    Update 00_KmerGenie.sh

--- 00_Assembly_Annotation/00_KmerGenie.sh
+++ 00_Assembly_Annotation/00_KmerGenie.sh
@@ -9,6 +9,7 @@
 module load anaconda2
 source activate kmergenie
 
+#predict genome size using Illumina raw reads
 kmergenie \
 #list_for_kmergenie contains the path to the Illumina raw data and I have generated it with:
 #readlink -f OSE_DAX-1_S55_L003_R* > list_for_kmergenie
