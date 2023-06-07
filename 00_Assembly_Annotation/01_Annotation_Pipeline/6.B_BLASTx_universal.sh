c39e59c83164c9d22f1bb1362e086839807aec61
Giacomo Moggioli <112566383+Moggiacomo@users.noreply.github.com>
2023-06-07 18:06:32 +0200

    Create 6.B_BLASTx_universal.sh

---
+++ 00_Assembly_Annotation/01_Annotation_Pipeline/6.B_BLASTx_universal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#$ -wd /data/scratch/btx654/
+#$ -o /data/scratch/btx654/
+#$ -j y
+#$ -pe smp 8
+#$ -l h_vmem=1G
+#$ -l h_rt=36:0:0
+
+species=$1
+
+echo "Working on "$species
+
+cd /data/scratch/btx654/btx604-scratch/$species/New_annotation_Dec2020/step8/
+
+module load anaconda3
+conda activate /data/SBCS-MartinDuranLab/03-Giacomo/src/anaconda3/trinotate_env
+
+blastx -query input_trinotate_mRNA.fa -db /data/SBCS-MartinDuranLab/03-Giacomo/db/trinotate/uniprot_sprot.pep -num_threads 8 -max_target_seqs 1 -outfmt 6 -evalue 1e-3 > blastx.outfmt6
