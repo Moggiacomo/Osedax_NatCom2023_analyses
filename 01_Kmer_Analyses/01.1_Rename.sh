edbcb12c6e26ed1ca72bebc64eb4c2a1cf528f9a
Giacomo Moggioli <112566383+Moggiacomo@users.noreply.github.com>
2023-06-07 18:34:13 +0200

    Create 01.1_Rename.sh

---
+++ 01_Kmer_Analyses/01.1_Rename.sh
@@ -0,0 +1,3 @@
+conda activate bbmap_env
+#osedax_pacbio_corrected.fasta was generated with CANU
+bbrename.sh in=osedax_pacbio_corrected.fasta out=osedax_pacbio_corrected_bbmap.fasta prefix=sequence
