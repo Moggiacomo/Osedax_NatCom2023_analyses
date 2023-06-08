1f3b3d65ef738a8a58a467b6e50d8b03bb44ed0b
Giacomo Moggioli <112566383+Moggiacomo@users.noreply.github.com>
2023-06-08 10:58:20 +0200

    Create 03.4_RBBH_2.sh

---
+++ 01_Other_Analyses/03_RiftiaPachyptila_Comparisons/03.4_RBBH_2.sh
@@ -0,0 +1,3 @@
+#using script found here: https://scriptomika.wordpress.com/2014/01/28/extract-best-reciprocal-blast-matches/
+
+../get_RBH.py blast.outfmt6 blast_reciprocal.outfmt6 1 2 11 low OliveiraVSGenome.hits.out
