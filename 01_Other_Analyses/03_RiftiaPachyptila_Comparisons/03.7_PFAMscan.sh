f6b92b16fb5bbdc4a97a84951924d7036251d89d
Giacomo Moggioli <112566383+Moggiacomo@users.noreply.github.com>
2023-06-08 11:05:13 +0200

    Create 03.7_PFAMscan.sh

---
+++ 01_Other_Analyses/03_RiftiaPachyptila_Comparisons/03.7_PFAMscan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -wd /data/scratch/btx654/pfam/pfamScan
+#$ -o /data/scratch/btx654/pfam/pfamScan
+#$ -j y
+#$ -l highmem
+#$ -pe smp 12
+#$ -l h_vmem=40G
+#$ -l h_rt=36:0:0
+#$ -l highmem
+
+module load anaconda3
+conda activate pfamScan_env
+
+pfam_scan.pl -cpu 12 -fasta ../RIFPA_final_gene_models_AUGUSTUS_v1_AGAT.prot.longestIsoform.fasta -dir /data/SBCS-MartinDuranLab/00-BlastDBs -outfile PFAMscan_oliveira2022NR.out
